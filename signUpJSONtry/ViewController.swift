18021e994e69a9a6c18cc9d912f38ccaa28d3059
Kanwar Sandhu <kanwarsandhu5255@gmail.com>
2018-04-20 14:18:15 -0700

    Initial Commit

---
+++ signUpJSONtry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  signUpJSONtry
+//
+//  Created by Kanwar Sudeep Singh Sandhu on 20/04/18.
+//  Copyright © 2018 Kanwar Sudeep Singh Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
